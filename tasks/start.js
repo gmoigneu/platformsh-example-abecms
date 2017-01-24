5ec21c604da38766e4619b8a8d314afd8da2bfe5
Emmanuel Gonzalez <emmanuel.gonzalez@adfab.fr>
2017-01-24 12:29:14 +0100

    Recipe-pm2 - 1.0

--- tasks/start.js
+++ tasks/start.js
@@ -1,25 +1,3 @@
-# recipe-pm2
-This Abe recipe demonstrates how to deploy your Abe project admin in production with [pm2](https://github.com/Unitech/pm2).
-
-## Add pm2 in dependencies in package.json
-
-This add pm2 in the project dependencies. 
-
-```javascript
-{
-  "dependencies": {
-     "pm2": "2.2.*",
-    },
-}
-``` 
-
-Run `npm i` to install dependencies.
-
-## Add a [npm](https://www.npmjs.com/) task to manage pm2 via npm.
-
-This task deal with pm2 process to start the process. If the pm2 process is already startn this tasks stop, delete the process an start the process.
-
-```javascript
 'use strict';
 
 var clc = require('cli-color');
@@ -82,39 +60,3 @@ pm2.connect((err) => {
     }
   });
 })
-```
-
-## Add the tasks in package.json
-
-This allow to add the npm task to run pm2 within your project.
-
-```javascript
-  "devDependencies": {},
-  "scripts": {
-    "test": "echo \"Error: no test specified\" && exit 1",
-    "start": "node path/to/start.js"
-  },
-```
-
-## Add pm2-config.json
-
-This is the config file for pm2. See pm2 process file [documentation](http://pm2.keymetrics.io/docs/usage/application-declaration/)
-
-```javascript
-{
-  "name": "pm2processname",
-  "script": "./node_modules/abe-cli/dist/server/app.js",
-  "args": "serve",
-  "nodeArgs": ["--harmony","--max-old-space-size=2048"], # You can pass node arguments here
-  "log_date_format" : "YYYY-MM-DD HH:mm Z", # Pm2 log format
-  "exec_mode": "fork", # Pm2 exec mode (fork or cluster)
-  "instances": "-1", # if cluster mode number of intances -1 = server number of cpus -1 
-  "env": {
-    "PORT": "8012" # local port to run Abe
-  }
-}
-```
-
-Then run `npm start`
-
-
